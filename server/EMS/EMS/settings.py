4514ec059870aef3ceee762516e66111d4c1a5b0
rajesh <rajeshrnair95@gmail.com>
2017-07-04 23:27:50 +0530

    added snippet class

--- server/EMS/EMS/settings.py
+++ server/EMS/EMS/settings.py
@@ -31,8 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'Gallery','Feedback'
+    'Gallery','Feedback',
     'Events','Register',
+    'rest_framework',
+    'snippets.apps.SnippetsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- server/EMS/Events/models.py
+++ server/EMS/Events/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+#class Events(models.Model):
+
+#   id = models.CharField(max_length = 50)
+#  description = models.CharField(max_length = 500)
+#   evnettitle = models.CharField(max_length = 50)
+#   venue = models.CharField(max_length = 50)
+#   contact = models.IntegerField()
+
+#   class Meta:
+#      db_table = "events"
